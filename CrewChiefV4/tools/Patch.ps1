3c325442af8d589f732d52c193c5d4b179c720a6
TheIronWolfModding <vleonavicius@hotmail.com>
2019-01-26 16:37:26 -0800

    Update patch.ps1 to include package dependencies.

--- CrewChiefV4/tools/Patch.ps1
+++ CrewChiefV4/tools/Patch.ps1
@@ -51,6 +51,14 @@ OverwriteFile $rootPath\"trackLandmarksData.json" $ccLayoutMainPath
 OverwriteFile $rootPath\"saved_command_macros.json" $ccLayoutMainPath
 OverwriteFile $rootPath\"speech_recognition_config.txt" $ccLayoutMainPath
 
+# Update binary dependencies.
+OverwriteFile $releaseBinPath\"AutoUpdater.NET.dll" $ccLayoutMainPath
+OverwriteFile $releaseBinPath\"MathNet.Numerics.dll" $ccLayoutMainPath
+OverwriteFile $releaseBinPath\"NAudio.dll" $ccLayoutMainPath
+OverwriteFile $releaseBinPath\"Newtonsoft.Json.dll" $ccLayoutMainPath
+OverwriteFile $releaseBinPath\"SharpDX.dll" $ccLayoutMainPath
+OverwriteFile $releaseBinPath\"SharpDX.DirectInput.dll" $ccLayoutMainPath
+
 echo "Press any key to finish..."
 
 Read-Host
