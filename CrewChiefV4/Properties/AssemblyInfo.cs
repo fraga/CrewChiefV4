bec97a50272b3c4187e6a2ef1c438b6317811b77
mrbelowski <jim.britton@yahoo.co.uk>
2020-08-31 09:00:52 +0100

    fixed app name fuckup

--- CrewChiefV4/Properties/AssemblyInfo.cs
+++ CrewChiefV4/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("5")]
+[assembly: AssemblyTitle("CrewChiefV4")]
 [assembly: AssemblyDescription("RaceRoom, PCars, PCars2, rFactor, rFactor2, Assetto Corsa, and iRacing race engineer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Britton IT Ltd")]
