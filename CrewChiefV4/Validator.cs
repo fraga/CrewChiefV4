572101a6694f0e7df5f48503fee16d43a1dc6839
mrbelowski <jim.britton@yahoo.co.uk>
2020-09-20 22:43:44 +0100

    more gender renames

--- CrewChiefV4/Validator.cs
+++ CrewChiefV4/Validator.cs
@@ -19,7 +19,7 @@ namespace CrewChiefV4
              "tim heinemann", "Josh Cassar", "Jesse Hoppo", "markus grönthal", "joan moreno", "N.Quinque", "epsilon",
              "^1!^5c^3hrist ^5i^3n ^5a^3ction", "slightly mad", "dickovens", "Pyromaniac", "luismat112", "Ruben Zukic", "Adam L Watson",
              "Dave Rowe"/*for brake checking doing daytona 24h and being a general dck*/, "Visar Gjikolli", "Lukasz Salwerowicz",
-             "xavi lópez" /*roughing up in ranked*/};
+             "xavi lópez", "bernie ecclestone" /*roughing up in ranked*/};
 
 
           public static void validate(String str)
