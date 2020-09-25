617e17cc026d93d8a6f199a00963a1b1968ee0a3
Jim Britton <jim.britton@yahoo.co.uk>
2020-09-25 07:18:28 +0000

    Splash image while loading

---
+++ CrewChiefV4/UserInterface/Loading.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrewChiefV4.UserInterface
+{
+    public partial class Loading : Form
+    {
+        public static string tempSplashImagePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CrewChiefV4\splash_image_tmp.png";
+        public static string splashImagePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\CrewChiefV4\splash_image.png";
+        public Loading()
+        {
+            InitializeComponent();
+        }
+    }
+}
