093d314dcdc2318adf7964c5797df3a114408fe7
TonyWhitley <tony_whitley@hotmail.com>
2020-10-15 14:14:11 +0000

    Add PitManagerVoiceCmds WIP Added autoFuelToEnd lifted from iRacing code WIP
    Added unit test and made a hesitant start on loading CC in it.

--- HelpFiles/Program.cs
+++ HelpFiles/Program.cs
@@ -1,18 +1,12 @@
-﻿//#define TextHelpFiles
-//#define FirstHTMLfiles
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using HtmlAgilityPack; // https://html-agility-pack.net/
 
 /// <summary>
-/// Create Help HTML files from
-/// * the menu bar menu.ht
-/// * the HTML pages from text (by appending <br> to each line)
-/// * the wrap up menu.ml
-/// * Output the results into /public from where they will be published
-///   in a GitLab page as well as being used by the Crew Chief "Help"
+/// Create Help HTML files by inserting each page into the menu bar menu.html
+/// Output the results into /public from where they will be published
+/// in a GitLab page linked to by the Crew Chief "Help"
 /// </summary>
 namespace HelpFiles
 {
@@ -20,7 +14,7 @@ namespace HelpFiles
     {
         static void Main(string[] args)
         {
-            string[] pageNames, gameNames;
+            string[] pageNames, gameNames, images;
             pageNames = new string []{
                 "index",
                 "GettingStarted_General",
@@ -69,6 +63,15 @@ namespace HelpFiles
                 "rFactor2",
                 "RichardBurnsRally"
             };
+            images = new string[]{
+                "styles.css",
+                "CrewChief.png",
+                "VoiceRecognition_InstallationTraining.png",
+                "engineer_edited.ico",
+                "engineer_edited_transparent.png",
+                "CC_Talk_to_CC.png",
+                "CC_Confidence.png"
+            };
 
             writeOnePage("index");
 
@@ -84,63 +87,11 @@ namespace HelpFiles
             }
 
             // Finally copy the css and the images
-            foreach (string file in new string[] { "styles.css",
-                "CrewChief.png",
-                "VoiceRecognition_InstallationTraining.png",
-                "engineer_edited.ico",
-                "engineer_edited_transparent.png",
-                "CC_Talk_to_CC.png",
-                "CC_Confidence.png"
-            })
+            foreach (string file in images)
             {
                 System.IO.File.Copy($"..\\..\\{file}", $"..\\..\\..\\public\\{file}", true);
             }
         }
-
-#if TextHelpFiles
-        static void writeOnePage(string pageName)
-        {
-            List<string> lines = System.IO.File.ReadAllLines("..\\..\\menu.ht").ToList();
-            List<string> text = System.IO.File.ReadAllLines($"..\\..\\{pageName}.txt").ToList();
-            List<string> wrapup = System.IO.File.ReadAllLines("..\\..\\menu.ml").ToList();
-
-            // Add the text to the menu boilerplate
-            foreach (string line in text)
-            {
-                if (pageName.StartsWith("About_ChangeLog") && line.StartsWith("Version"))
-                    lines.Add($"<h4>{line}</h4>");  // Version 4.11.1.2 -> <h4>Version 4.11.1.2</h4>
-                else if (line.StartsWith("<"))
-                    lines.Add($"{line}");   // No <br> on HTML-tagged lines
-                else // replace leading spaces with &nbsp; then copy the rest plus <br>
-                {   // (Neater using regex substitution but tricky to work out)
-                    string _newLine = String.Empty;
-                    for (var i = 0; i < line.Length; i++)
-                    {
-                        if (line[i] == ' ')
-                        {
-                            _newLine += "&nbsp;";
-                        }
-                        else
-                        {
-                            _newLine += line.Substring(i);
-                            break;
-                        }
-                    }
-                    lines.Add($"{_newLine}<br>");
-                }
-            }
-
-            // Add the wrap up
-            foreach (string line in wrapup)
-            {
-                lines.Add(line);
-            }
-
-            // Write the result
-            System.IO.File.WriteAllLines($"..\\..\\..\\public\\{pageName}.html", lines);
-            Console.WriteLine($"{pageName}.html written");
-        }
-#endif
 
         /// <summary>
         /// Insert the content in the HTML file between the menu and
@@ -162,28 +113,5 @@ namespace HelpFiles
             oldChild.InnerHtml = insert;
             templateDoc.Save($"..\\..\\..\\public\\{pageName}.html");
         }
-
-#if FirstHTMLfiles
-        /// <summary>
-        /// ONLY USED ONCE TO CREATE *.HTML
-        /// Create a replacement for the .txt file by inserting the HTML
-        /// text section (<div>) from the original help file into a blank HTML
-        /// </summary>
-        /// <param name="pageName"></param>
-        static void writeOnePage(string pageName)
-        {
-            var templateDoc = new HtmlDocument();
-            templateDoc.Load("..\\..\\blank.html");
-            var node = templateDoc.DocumentNode.SelectSingleNode("//div");
-            var nodes = templateDoc.DocumentNode.SelectNodes("//div");
-            var oldChild = nodes[1];
-
-            var contentDoc = new HtmlDocument();
-            contentDoc.Load($"..\\..\\..\\public\\{pageName}.html");
-            var insert = contentDoc.DocumentNode.SelectSingleNode("//div").InnerHtml;
-            oldChild.InnerHtml = insert;
-            templateDoc.Save($"..\\..\\{pageName}.html");
-        }
-#endif
     }
 }
