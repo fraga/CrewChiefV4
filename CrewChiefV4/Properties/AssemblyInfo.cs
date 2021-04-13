ed0c2d509bc2ec29cfe1fb0fd9d719411f9fb5b2
mrbelowski <jim.britton@yahoo.co.uk>
2021-04-13 09:17:09 +0100

    update stuff

--- CrewChiefV4/Properties/AssemblyInfo.cs
+++ CrewChiefV4/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.15.2.3")]
+[assembly: AssemblyVersion("4.15.2.4")]
 
 [assembly: AssemblyFileVersion("1.0.0.0")]
