1c5e3c591eb64f5b9a4f60fa3da326613b15aaca
TonyWhitley <Tony_Whitley@hotmail.com>
2022-09-24 11:57:54 +0100

    rf2_enable_auto_clear_tyre_change Clears rF2 pit menu tyre change at start of race

--- CrewChiefV4/Properties/Settings.Designer.cs
+++ CrewChiefV4/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CrewChiefV4.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -5432,6 +5432,18 @@ namespace CrewChiefV4.Properties {
             }
             set {
                 this["pause_between_message_fragments"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool rf2_enable_auto_clear_tyre_change {
+            get {
+                return ((bool)(this["rf2_enable_auto_clear_tyre_change"]));
+            }
+            set {
+                this["rf2_enable_auto_clear_tyre_change"] = value;
             }
         }
     }
